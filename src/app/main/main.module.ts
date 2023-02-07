0ca829a8b86b3d41f617fe352f1ca460b66a1c66
Artur Rios <artur.oliveira@pantheon-inc.com>
2023-02-07 19:51:51 -0300

    feat: develop home component and add tailwind

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,15 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
 import { HomeComponent } from './home/home.component';
+import { TableFormComponent } from './table-form/table-form.component';
 
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [
-    CommonModule,
-    MainRoutingModule
-  ]
+  declarations: [HomeComponent, TableFormComponent],
+  imports: [CommonModule, MainRoutingModule, MatButtonModule],
 })
-export class MainModule { }
+export class MainModule {}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {},
+    colors: {
+      accent: "#303030",
+      black: "#000",
+      primary: "#e91e63",
+      warn: "#ff0000",
+      white: "#fff",
+    },
+  },
+  plugins: [],
+};
